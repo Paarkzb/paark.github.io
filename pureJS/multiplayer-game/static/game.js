2e049f6fe5afe7444b97593520558590a03dbc26
Zorikto Baldanov <45837027+Paarkzb@users.noreply.github.com>
2022-11-05 01:36:45 +0800

    demo

---
+++ pureJS/multiplayer-game/static/game.js
@@ -0,0 +1,68 @@
+let socket = io();
+
+let movement = {
+    up: false,
+    down: false,
+    left: false,
+    right: false,
+};
+
+document.addEventListener("keydown", function (event) {
+    switch (event.keyCode) {
+        case 65: // A
+            movement.left = true;
+            break;
+        case 87: // W
+            movement.up = true;
+            break;
+        case 68: // D
+            movement.right = true;
+            break;
+        case 83: // S
+            movement.down = true;
+            break;
+    }
+});
+
+document.addEventListener("keyup", function (event) {
+    switch (event.keyCode) {
+        case 65: // A
+            movement.left = false;
+            break;
+        case 87: // W
+            movement.up = false;
+            break;
+        case 68: // D
+            movement.right = false;
+            break;
+        case 83: // S
+            movement.down = false;
+            break;
+    }
+});
+
+socket.emit("new player");
+
+setInterval(function () {
+    socket.emit("movement", movement);
+}, 1000 / 60);
+
+let canvas = document.getElementById("canvas");
+canvas.width = 800;
+canvas.height = 600;
+let ctx = canvas.getContext("2d");
+
+socket.on("state", function (players) {
+    ctx.clearRect(0, 0, 800, 600);
+    ctx.fillStyle = "green";
+
+    for (let id in players) {
+        let player = players[id];
+
+        // console.log(players);
+
+        ctx.beginPath();
+        ctx.arc(player.x, player.y, 10, 0, 2 * Math.PI);
+        ctx.fill();
+    }
+});
